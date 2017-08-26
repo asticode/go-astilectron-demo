312e74675ed28f0a7ee2590427afb7efdfd05db1
Quentin Renard <contact@asticode.com>
2017-08-26 17:44:22 +0200

    Added list of files in home directory

--- resources/app/static/js/index.js
+++ resources/app/static/js/index.js
@@ -4,6 +4,9 @@ var index = {
         document.addEventListener('astilectron-ready', function() {
             // Listen
             index.listen();
+
+            // Refresh list
+            index.refreshList();
         })
     },
     listen: function() {
@@ -17,5 +20,18 @@ var index = {
     },
     listenSetStyle: function(message) {
         document.body.className = message.payload;
+    },
+    refreshList: function() {
+        astilectron.send({"name": "get.list"}, function(message) {
+            if (message.payload.length === 0) {
+                return
+            }
+            let c = `<ul>`
+            for (let i = 0; i < message.payload.length; i++) {
+                c += `<li class="` + message.payload[i].type + `">` + message.payload[i].name + `</li>`
+            }
+            c += `</ul>`
+            document.getElementById("list").innerHTML = c
+        })
     }
 };
