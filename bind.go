f3fc2748a81bfdd5421454425eded9215f774642
Quentin Renard <contact@asticode.com>
2018-04-22 11:28:42 +0100

    Fixed go get

--- bind.go
+++ bind.go
@@ -4,6 +4,10 @@ func Asset(name string) ([]byte, error) {
 	return []byte{}, nil
 }
 
+func AssetDir(name string) ([]string, error) {
+	return []string{}, nil
+}
+
 func RestoreAssets(dir, name string) error {
 	return nil
 }
